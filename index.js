f0d53bb972da36922e67cd0d4b2244f0fe78d266
Marwa Sh <marwa.al.shahal@gmail.com>
2020-12-16 11:21:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Dinner {
   constructor (salad,soup,entree,dessert) {
     this.salad=salad;
     this.soup=soup;
-    thid.entree=entree;
+    this.entree=entree;
     this._dessert=dessert;
   }
 }
